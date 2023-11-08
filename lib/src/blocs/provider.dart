de96dfd79e4cc08f66124b8fd3d20ab0a2ab5b01
Rodrigo Bossini <professorbossini@gmail.com>
2023-11-08 12:51:28 -0300

    ap02(bloc): pagina 52, vimos covariant

---
+++ lib/src/blocs/provider.dart
@@ -0,0 +1,7 @@
+// import 'package:flutter/material.dart';
+// import 'bloc.dart';
+
+// class Provider extends InheritedWidget{
+//   @override
+//   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+// }
